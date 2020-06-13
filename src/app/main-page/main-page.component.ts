ae66e4f3d686c728b6680919b2a251a0b1df0464
Saniahmetov Ruslan <colorful.soul.777@gmail.com>
2020-06-13 19:54:59 +0500

    fix main page

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { SignInDialogComponent } from '../sign-in-dialog/sign-in-dialog.component';
 
 @Component({
   selector: 'app-main-page',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private dialog: MatDialog
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  openSignInDialog() {
+    this.dialog.open(SignInDialogComponent);
   }
 
 }
