b0d63776159ba589b1f14954a6338e4d1dc8da68
Saniahmetov Ruslan <colorful.soul.777@gmail.com>
2020-06-14 10:13:08 +0500

    contractors page changed

--- src/app/contractors-page/contractors-page.component.ts
+++ src/app/contractors-page/contractors-page.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Contractor {
+  name: string;
+  address: string;
+}
+
 @Component({
   selector: 'app-contractors-page',
   templateUrl: './contractors-page.component.html',
@@ -7,7 +12,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContractorsPageComponent implements OnInit {
 
-  constructor() { }
+  contractors: Contractor[] = [
+    {
+      name: 'ИП "Собакофф"',
+      address: 'г. Бугуруслан, ул. Московская, д. 12',
+    },
+    {
+     name: 'ИП "Собакофф"',
+      address: 'г. Бугуруслан, ул. Московская, д. 12',
+    },
+    {
+     name: 'ИП "Собакофф"',
+      address: 'г. Бугуруслан, ул. Московская, д. 12',
+    },
+    {
+      name: 'ИП "Собакофф"',
+      address: 'г. Бугуруслан, ул. Московская, д. 12',
+    },
+    {
+      name: 'ИП "Собакофф"',
+      address: 'г. Бугуруслан, ул. Московская, д. 12',
+    },
+    {
+      name: 'ИП "Собакофф"',
+      address: 'г. Бугуруслан, ул. Московская, д. 12',
+    },
+  ];
+
+  statisticYear = 2020;
+
+  constructor(
+  ) {
+  }
 
   ngOnInit(): void {
   }
--- src/app/contractors-page/contractors-page.module.ts
+++ src/app/contractors-page/contractors-page.module.ts
@@ -3,13 +3,25 @@ import { CommonModule } from '@angular/common';
 
 import { ContractorsPageRoutingModule } from './contractors-page-routing.module';
 import { ContractorsPageComponent } from './contractors-page.component';
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { CardsModule } from '../cards/cards.module';
 
 
 @NgModule({
   declarations: [ContractorsPageComponent],
   imports: [
     CommonModule,
-    ContractorsPageRoutingModule
-  ]
+    ContractorsPageRoutingModule,
+    FormsModule,
+    MatCardModule,
+    MatIconModule,
+    MatMenuModule,
+    MatButtonModule,
+    CardsModule,
+  ],
 })
 export class ContractorsPageModule { }
