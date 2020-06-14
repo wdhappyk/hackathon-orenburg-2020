a0655c19d2fba1cfa8a4388855cf44047adb49fb
Saniahmetov Ruslan <colorful.soul.777@gmail.com>
2020-06-14 05:05:30 +0500

    photo viewer changed

--- src/app/photo-viewer/photo-viewer/photo-viewer.component.ts
+++ src/app/photo-viewer/photo-viewer/photo-viewer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-photo-viewer',
@@ -6,6 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./photo-viewer.component.scss']
 })
 export class PhotoViewerComponent implements OnInit {
+
+  selected: string;
+  photoList = [];
+
+  @Input()
+  set photos(photos: string[]) {
+    this.photoList = photos;
+    this.selected = photos[0];
+  }
 
   constructor() { }
 
