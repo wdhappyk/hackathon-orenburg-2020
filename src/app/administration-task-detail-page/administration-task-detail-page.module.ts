c8612a0690d851c4a03246058297b26416849c18
Saniahmetov Ruslan <colorful.soul.777@gmail.com>
2020-06-14 09:57:05 +0500

    task detail page complete

--- src/app/administration-task-detail-page/administration-task-detail-page.module.ts
+++ src/app/administration-task-detail-page/administration-task-detail-page.module.ts
@@ -3,13 +3,36 @@ import { CommonModule } from '@angular/common';
 
 import { AdministrationTaskDetailPageRoutingModule } from './administration-task-detail-page-routing.module';
 import { AdministrationTaskDetailPageComponent } from './administration-task-detail-page.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatSelectModule } from '@angular/material/select';
+import { CardsModule } from '../cards/cards.module';
+import { PhotoViewerModule } from '../photo-viewer/photo-viewer.module';
+import { MatNativeDateModule } from '@angular/material/core';
 
 
 @NgModule({
   declarations: [AdministrationTaskDetailPageComponent],
   imports: [
     CommonModule,
-    AdministrationTaskDetailPageRoutingModule
-  ]
+    AdministrationTaskDetailPageRoutingModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatSelectModule,
+    CardsModule,
+    PhotoViewerModule,
+    MatNativeDateModule,
+  ],
 })
-export class AdministrationTaskDetailPageModule { }
+export class AdministrationTaskDetailPageModule {
+}
