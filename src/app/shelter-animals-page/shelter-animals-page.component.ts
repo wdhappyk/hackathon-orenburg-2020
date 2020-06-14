96a6881229b701e3111aa5a2993f0a3e0dd008e8
Saniahmetov Ruslan <colorful.soul.777@gmail.com>
2020-06-14 12:44:56 +0500

    add animals of shelter page

--- src/app/shelter-animals-page/shelter-animals-page.component.ts
+++ src/app/shelter-animals-page/shelter-animals-page.component.ts
@@ -1,15 +1,75 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { ConfirmDialogService } from '../confirm-dialog/confirm-dialog.service';
+
+interface Animal {
+  category: string;
+  mark: string;
+  name: string;
+  virtualOwnerExists: boolean;
+  date: Date;
+}
 
 @Component({
   selector: 'app-shelter-animals-page',
   templateUrl: './shelter-animals-page.component.html',
-  styleUrls: ['./shelter-animals-page.component.scss']
+  styleUrls: ['./shelter-animals-page.component.scss'],
 })
 export class ShelterAnimalsPageComponent implements OnInit {
 
-  constructor() { }
+
+  get filteredAnimals(): Animal[] {
+    const categories = this.categoryFormControl.value?.length ? this.categoryFormControl.value : ['dogs', 'cats'];
+    return this.animals.filter((animal) => {
+      return categories.includes(animal.category);
+    });
+  };
+
+  animals: Animal[] = [
+    {
+      category: 'dogs',
+      mark: '1231235',
+      name: 'Артурик',
+      virtualOwnerExists: false,
+      date: new Date(2020, 4, 15),
+    },
+    {
+      category: 'dogs',
+      mark: '1231235',
+      name: 'Артурик',
+      virtualOwnerExists: false,
+      date: new Date(2020, 4, 15),
+    },
+    {
+      category: 'dogs',
+      mark: '1231235',
+      name: 'Артурик',
+      virtualOwnerExists: false,
+      date: new Date(2020, 4, 15),
+    },
+    {
+      category: 'dogs',
+      mark: '1231235',
+      name: 'Артурик',
+      virtualOwnerExists: false,
+      date: new Date(2020, 4, 15),
+    },
+  ];
+  categoryFormControl = new FormControl([]);
+
+  constructor(
+    private confirmDialog: ConfirmDialogService
+  ) {
+  }
 
   ngOnInit(): void {
   }
 
+  removeAnimal(animal: any) {
+    this.confirmDialog.open('Вы уверены?', 'Вы действительно хотите удалить животное?').subscribe((value => {
+      if (!value) return;
+      const idx = this.animals.indexOf(animal);
+      this.animals.splice(idx, 1);
+    }));
+  }
 }
--- src/app/shelter-animals-page/shelter-animals-page.module.ts
+++ src/app/shelter-animals-page/shelter-animals-page.module.ts
@@ -3,13 +3,39 @@ import { CommonModule } from '@angular/common';
 
 import { ShelterAnimalsPageRoutingModule } from './shelter-animals-page-routing.module';
 import { ShelterAnimalsPageComponent } from './shelter-animals-page.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CardsModule } from '../cards/cards.module';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
 
 
 @NgModule({
   declarations: [ShelterAnimalsPageComponent],
   imports: [
     CommonModule,
-    ShelterAnimalsPageRoutingModule
-  ]
+    ShelterAnimalsPageRoutingModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatMenuModule,
+    MatDividerModule,
+    MatIconModule,
+    MatButtonToggleModule,
+    ReactiveFormsModule,
+    CardsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+  ],
 })
 export class ShelterAnimalsPageModule { }
